b540732975db81fa327663069af3fc1c4311d8b9
Thiago Macieira <thiago.macieira@intel.com>
2023-05-22 18:17:19 -0700

    Short live q20::atomic_wait!
    atomic_wait() and atomic_notify_{one,all}() are available in C++20,
    which we can't depend on right now. So we implement our own fallback
    implementation. This is a simple implementation for systems which don't
    have either futexes or the C++20 atomic wait API. That means it's not
    very efficient, just simple. The unit test tests the fallback
    implementation only: it is not Qt's business to test the Standard
    Library.
    
    The fallback implementation and the Standard Library's are not
    binary-compatible and cannot be mixed. Callers must ensure that all
    sides use the same implementation and the easiest way to do that is to
    only use this in non-inline code, or at worst inline code that isn't
    shared across libraries/modules/plugins.
    
    Change-Id: Ib5ce7a497e034ebabb2cfffd1761a0e497dd17d4
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>

---
+++ src/corelib/thread/qatomicwait.cpp
@@ -0,0 +1,164 @@
+// Copyright (C) 2025 Intel Corporation.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qatomicwait_p.h"
+
+#include <qendian.h>
+#include "qwaitcondition_p.h"
+
+#include <array>
+#include <condition_variable>
+#include <mutex>
+
+QT_BEGIN_NAMESPACE
+
+/*
+ * Implementation details:
+ *
+ * Normally, a call from atomic_notify_one() or atomic_notify_all() corresponds
+ * to a call to cond.notify_one() or cond.notify_all(). This simple
+ * implementation would require that we keep a distinct wait condition variable
+ * per atomic variable, of which there could be an arbitrary number.
+ *
+ * Instead, we have a limited set of std::condition_variable, which are
+ * selected based on the address of the variable being waited/notified on.
+ * Because multiple, distinct variables could be sharing the same condition
+ * variable, we need to perform a cond.notify_all() call in case the lock is
+ * contended by waiters to more than one address, as we can't be sure which one
+ * would be woken up by the cond.notify_one() call.
+ *
+ * We recover some of the performance of notifying a single waker by also
+ * storing the address of the variable that was being waited on. If it matches
+ * the address of the variable being notified, we can perform a notify_one()
+ * call. This also allows us to avoid any system call in case no waiter has yet
+ * joined the queue. In case of contention, we store a sentinel address
+ * instead, indicating there are waiters to multiple variables. The last waiter
+ * to leave a wait queue is responsible for resetting the watched address back
+ * to nullptr.
+ *
+ * Performance details:
+ *
+ * This implementation is designed for systems where neither the Standard
+ * Library's own std::atomic_wait nor operating system futexes are available
+ * (read: usually, not the systems Qt is used most frequently on). Therefore,
+ * it's designed for simplicity, not performance. Simplifications applied
+ * include:
+ * - the total number of possible condition variables
+ * - the hashing algorithm to select a condition variable (simple XOR)
+ * - no attempt to wait for a variable change before calling cond.wait()
+ *   (no spinning, no HW-assisted wait)
+ *
+ * Other limitations:
+ *
+ * We only support 8-, 16-, 32- and 64-bit variables and we require bit-exact
+ * comparisons for all bits. This means we do not support objects with padding
+ * bits or without unique representations.
+ */
+
+namespace {
+struct QAtomicWaitLocks
+{
+    // Sentinel address used to indicate this Lock is being used by waiters to
+    // multiple addresses, implying we must make a notify_all() call.
+    static void *contendedWatchAddress() { return reinterpret_cast<void *>(-1); }
+
+    struct Lock {
+        alignas(QtPrivate::IdealMutexAlignment) std::mutex mutex;
+        alignas(QtPrivate::IdealMutexAlignment) std::condition_variable cond;
+
+        // Can assume values:
+        // - nullptr: no waiter is waiting
+        // - contendedWatchAddress(): waiters to distinct addresses
+        // - any other value: all waiters are waiting on the same address
+        const void *watchedAddress = nullptr;
+        qsizetype watcherCount = 0;
+    };
+
+    static constexpr int LockCount = 16;
+    std::array<Lock, LockCount> locks;
+
+    int indexFor(const void *ptr)
+    {
+        static_assert((LockCount & (LockCount - 1)) == 0, "LockCount is not a power of two");
+
+        quintptr value = quintptr(ptr) / sizeof(int);
+        quintptr idx = value % LockCount;
+#ifndef QATOMICWAIT_USE_FALLBACK
+        // XOR some higher bits too to avoid hashing collisions
+        // (we don't do it in the unit test because we *want* those collisions)
+        idx ^= (value / LockCount) % LockCount;
+        idx ^= (value / LockCount / LockCount) % LockCount;
+#endif
+        return int(idx);
+    }
+
+    Lock &lockFor(const void *ptr) { return locks[indexFor(ptr)]; }
+};
+} // unnamed namespace
+
+static QAtomicWaitLocks &atomicLocks() noexcept
+{
+    static QAtomicWaitLocks global {};
+    return global;
+}
+
+template <typename T> static bool isEqual(const void *address, const void *old)
+{
+    auto atomic = static_cast<const std::atomic<T> *>(address);
+    auto expected = static_cast<const T *>(old);
+    return atomic->load(std::memory_order_relaxed) == *expected;
+}
+
+static bool isEqual(const void *address, const void *old, qsizetype size) noexcept
+{
+    switch (size) {
+    case 1: return isEqual<quint8 >(address, old);
+    case 2: return isEqual<quint16>(address, old);
+    case 4: return isEqual<quint32>(address, old);
+    case 8: return isEqual<quint64>(address, old);
+    }
+    Q_UNREACHABLE_RETURN(false);
+}
+
+void QtPrivate::_q_atomicWait(const void *address, const void *old, qsizetype size) noexcept
+{
+    auto &locker = atomicLocks().lockFor(address);
+
+    // NOT noexcept; we'll terminate if locking the mutex fails
+    std::unique_lock lock(locker.mutex);
+
+    // is the value still current?
+    if (!isEqual(address, old, size))
+        return;                     // no, failed to wait
+
+    if (locker.watchedAddress && locker.watchedAddress != address)
+        locker.watchedAddress = QAtomicWaitLocks::contendedWatchAddress();
+    else
+        locker.watchedAddress = address;
+    ++locker.watcherCount;
+
+    do {
+        locker.cond.wait(lock);
+    } while (isEqual(address, old, size));
+
+    if (--locker.watcherCount == 0)
+        locker.watchedAddress = nullptr;
+}
+
+void QtPrivate::_q_atomicWake(void *address, WakeMode mode) noexcept
+{
+    auto &locker = atomicLocks().lockFor(address);
+
+    // NOT noexcept; we'll terminate if locking the mutex fails
+    std::unique_lock lock(locker.mutex);
+
+    // can we wake just one?
+    if (mode == WakeMode::One && locker.watchedAddress == address)
+        locker.cond.notify_one();
+    else if (locker.watchedAddress != nullptr)
+        locker.cond.notify_all();
+    else
+        qt_noop();          // no one was waiting
+}
+
+QT_END_NAMESPACE
---
+++ tests/auto/corelib/thread/qatomicwait/tst_qatomicwait.cpp
@@ -0,0 +1,303 @@
+// Copyright (C) 2025 Intel Corporation.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+
+#include <private/qatomicwait_p.h>
+
+#include <QRandomGenerator>
+#include <QSemaphore>
+#include <QThread>
+
+#include <QtTest>
+
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+using namespace std::chrono_literals;
+using namespace QtFallbackAtomicWait;
+
+constexpr int Repeats = 9;
+constexpr int MaxThreads = 8;
+constexpr auto Delay = 10ms;
+
+// moc can expand this
+#define DEF_TEST(name)                          \
+    void name ## _int() { name<int>(); }        \
+    void name ## _qint64() { name<qint64>(); }  \
+    void name ## _qint8() { name<qint8>(); }    \
+    void name ## _voidp() { name<void *>(); }   \
+    /* */
+
+class tst_QAtomicWait : public QObject
+{
+    Q_OBJECT
+private slots:
+    DEF_TEST(noWaiterWake)
+    DEF_TEST(failedWait)
+    DEF_TEST(singleWaiterWakeOne)
+    DEF_TEST(singleWaiterWakeAll)
+    DEF_TEST(multiWaiterWakeAll)
+    DEF_TEST(multiWaiterWakeSequentially)
+    DEF_TEST(aliasingTest)
+
+private:
+    template <typename T> void noWaiterWake();
+    template <typename T> void failedWait();
+    template <typename T> void singleWaiterWakeOne();
+    template <typename T> void singleWaiterWakeAll();
+    template <typename T> void multiWaiterWakeAll();
+    template <typename T> void multiWaiterWakeSequentially();
+    template <typename T> void aliasingTest();
+};
+
+class SimpleLatch
+{
+    std::mutex mutex;
+    int counter = 0;
+    std::condition_variable cond;
+
+public:
+    SimpleLatch(int n) : counter(n) {}
+
+    void countDown(int n = 1)
+    {
+        std::unique_lock l(mutex);
+        counter -= n;
+        if (counter == 0)
+            cond.notify_all();
+    }
+
+    void wait()
+    {
+        std::unique_lock l(mutex);
+        if (counter)
+            cond.wait(l);
+    }
+};
+
+class JThread
+{
+    std::thread thr;
+public:
+    template <typename... Args> JThread(Args &&... args)
+        : thr(std::forward<Args>(args)...)
+    { }
+
+    ~JThread()
+    {
+        if (thr.joinable())
+            thr.join();
+    }
+
+    void wait() { thr.join(); }
+};
+
+template <typename T> static T otherValue(int i = 1)
+{
+    if constexpr (std::is_pointer_v<T>) {
+        return reinterpret_cast<T>(otherValue<quintptr>(i));
+    } else if constexpr (sizeof(T) > sizeof(int)) {
+        return (T(i) << 32) + ~T(i);
+    } else {
+        return T(i);
+    }
+}
+
+template <typename T> static T badValue()
+{
+    if constexpr (std::is_pointer_v<T>) {
+        return reinterpret_cast<T *>(0xfefefefe);
+    } else {
+        return T(0xfefefefe);
+    }
+}
+
+template <typename T> void spinlockWait(std::atomic<T> &a)
+{
+    while (a.load(std::memory_order_relaxed) == T{}) {
+        qYieldCpu();
+    }
+}
+
+template <typename T> void tst_QAtomicWait::noWaiterWake()
+{
+    // Verify that there's no side-effect when there are no waiters. These
+    // tests should do absolutely nothing (not even call the out-of-line
+    // function).
+    std::atomic<T> a{};
+    q20::atomic_notify_one(&a);
+    q20::atomic_notify_all(&a);
+}
+
+template<typename T>
+void tst_QAtomicWait::failedWait()
+{
+    // Verify that we don't go into wait if the value in the atomic is
+    // different from the value we expected to wait on.
+
+    std::atomic<T> a{};
+    q20::atomic_wait_explicit(&a, otherValue<T>(), std::memory_order_relaxed);
+
+    a.store(otherValue<T>(), std::memory_order_relaxed);
+    q20::atomic_wait_explicit(&a, {}, std::memory_order_relaxed);
+}
+
+template <typename T, void (*wakeFn)(std::atomic<T> *a)> static void singleWaiterWake()
+{
+    std::atomic<T> a{};
+
+    for (int repetition = 0; repetition < Repeats; ++repetition) {
+        SimpleLatch latch(1);
+        a.store({}, std::memory_order_relaxed);
+
+        JThread thr([&] {
+            latch.countDown();
+            QTest::qSleep(Delay);
+            a.store(otherValue<T>());
+            wakeFn(&a);
+        });
+
+        // wait for the thread to start
+        latch.wait();
+
+        // wait for the atomic to change
+        // Note: it may have already changed if the qSleep() above has already
+        // finished.
+        q20::atomic_wait_explicit(&a, {}, std::memory_order_relaxed);
+
+        QCOMPARE(a.load(), otherValue<T>());
+    }
+}
+
+template <typename T> void tst_QAtomicWait::singleWaiterWakeOne()
+{
+    singleWaiterWake<T, q20::atomic_notify_one<T>>();
+}
+
+template <typename T> void tst_QAtomicWait::singleWaiterWakeAll()
+{
+    singleWaiterWake<T, q20::atomic_notify_all<T>>();
+}
+
+template <typename T, typename Wake> void multiWaiterWake(Wake &&wakeFn)
+{
+    std::atomic<T> a{};
+
+    for (int repetition = 0; repetition < Repeats; ++repetition) {
+        std::array<T, MaxThreads> loadedValues;
+        loadedValues.fill(badValue<T>());
+        a.store({}, std::memory_order_relaxed);
+
+        {
+            std::unique_ptr<JThread> threads[MaxThreads];
+            SimpleLatch latch(MaxThreads);
+            auto l = [&](T *loadedValue) {
+                latch.countDown();
+                q20::atomic_wait_explicit(&a, {}, std::memory_order_relaxed);
+                *loadedValue = a.load(std::memory_order_relaxed);
+            };
+
+            for (int i = 0; i < MaxThreads; ++i)
+                threads[i] = std::make_unique<JThread>(l, &loadedValues[i]);
+
+            // wait for the threads to start
+            latch.wait();
+
+            QTest::qSleep(Delay);
+            a.store(otherValue<T>());
+            wakeFn(a);
+
+            // wait for the threads to finish
+        }
+
+        std::array<T, MaxThreads> expected;
+        expected.fill(otherValue<T>());
+        QCOMPARE(loadedValues, expected);
+    }
+}
+
+template <typename T> void tst_QAtomicWait::multiWaiterWakeAll()
+{
+    multiWaiterWake<T>([](std::atomic<T> &a) {
+        q20::atomic_notify_all(&a);
+    });
+}
+
+template <typename T> void tst_QAtomicWait::multiWaiterWakeSequentially()
+{
+    multiWaiterWake<T>([](std::atomic<T> &a) {
+        auto delay = Delay;
+        for (int i = 0; i < MaxThreads; ++i) {
+            q20::atomic_notify_one(&a);
+            delay /= 4;
+            if (delay > 0ms)
+                QTest::qSleep(delay);
+        }
+    });
+}
+
+template <typename T> void tst_QAtomicWait::aliasingTest()
+{
+    // There is a limited number of locks available, so let's try atomics whose
+    // addresses are very similar. We can't ask for high alignment values on
+    // the stack in all platforms, so we just pad the structure to 256 bytes.
+    struct Atomics {
+        std::atomic<T> a;
+        T loadedValue;
+        void reset()
+        {
+            a.store({}, std::memory_order_relaxed);
+            loadedValue = badValue<T>();
+        }
+    };
+    struct Padded : Atomics {
+        char padding[256 - sizeof(Atomics)];
+    };
+    static_assert(sizeof(Padded) == 256);
+    Padded atomics[MaxThreads]{};
+
+    for (int repetition = 0; repetition < Repeats; ++repetition) {
+        for (Atomics &s : atomics)
+            s.reset();
+
+        std::array<int, MaxThreads> order;
+        std::iota(order.begin(), order.end(), 0);
+        std::shuffle(order.begin(), order.end(), *QRandomGenerator::global());
+
+        std::unique_ptr<JThread> threads[MaxThreads];
+        SimpleLatch latch(MaxThreads);
+        auto l = [&](Atomics *atomic) {
+            latch.countDown();
+            q20::atomic_wait_explicit(&atomic->a, {}, std::memory_order_relaxed);
+            atomic->loadedValue = atomic->a.load(std::memory_order_relaxed);
+        };
+        for (int i = 0; i < MaxThreads; ++i)
+            threads[i] = std::make_unique<JThread>(l, &atomics[order[i]]);
+
+        // wait for the threads to start
+        latch.wait();
+
+        QTest::qSleep(Delay);
+
+        std::array<T, MaxThreads> expected, actual;
+        actual.fill({});
+        expected.fill({});
+        for (int i = 0; i < MaxThreads; ++i) {
+            expected[i] = otherValue<T>(i + MaxThreads);
+            Atomics &atomic = atomics[order[i]];
+            atomic.a.store(expected[i], std::memory_order_relaxed);
+            q20::atomic_notify_one(&atomic.a);
+
+            // wait for this thread
+            threads[i]->wait();
+
+            actual[i] = atomic.loadedValue;
+            QCOMPARE(actual, expected);
+        }
+    }
+}
+
+QTEST_MAIN(tst_QAtomicWait)
+
+#include "tst_qatomicwait.moc"
+
