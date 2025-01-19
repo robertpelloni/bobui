eb3df4edbc0ca04a892728e54962ad0daef4a78e
Marc Mutz <marc.mutz@qt.io>
2025-01-19 09:22:58 +0100

    Long live Q_PRE/Q_PRE_X!
    As decided with lazy consensus on the development ML¹, we'd like to
    separate precondition checking from internal consistency checking.
    
    Add a new Q_PRE/Q_PRE_X macro for the former, to leave Q_ASSERT/_X for
    the latter.
    
    As requested in review, modernize Q_PRE_X vis-a-vis Q_ASSERT_X by
    skipping the `where` parameter (defaulting it to Q_FUNC_INFO).
    
    Added as undocumented API, for now, to pick back to all active
    branches without actually promising semantics different from Q_ASSERT,
    a change that, realistically, will only happen for 6.10 at the
    earliest. But by making the macro available to all active branches, we
    avoid conflicts when picking changes back, and the change is rather
    risk-less.
    
    Apply to QStringView, to have at least one user.
    
    ¹ https://lists.qt-project.org/pipermail/development/2024-August/045588.html Items 1-3.
    
    Pick-to: 6.9 6.8 6.5
    Task-number: QTBUG-98965
    Change-Id: Ia04248a64c8feba80cce10f8f5cbde580436db88
    Reviewed-by: Jøger Hansegård <joger.hansegard@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/corelib/global/qassert.h
+++ src/corelib/global/qassert.h
@@ -48,6 +48,16 @@ inline bool qt_no_assert_x(bool, const char *, const char *) noexcept { return f
 #  endif
 #endif
 
+#ifndef Q_PRE
+# define Q_PRE(cond) \
+    Q_ASSERT(cond) /* for now... */
+#endif
+
+#ifndef Q_PRE_X
+# define Q_PRE_X(cond, what) \
+    Q_ASSERT_X(cond, Q_FUNC_INFO, what) /* for now... */
+#endif
+
 Q_NORETURN Q_CORE_EXPORT void qt_check_pointer(const char *, int) noexcept;
 Q_NORETURN Q_DECL_COLD_FUNCTION
 Q_CORE_EXPORT void qBadAlloc();
