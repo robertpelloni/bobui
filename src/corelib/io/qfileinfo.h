9a83c2b88c13a97ea170edb3d9aa06757aa387d9
Ahmad Samir <a.samirh78@gmail.com>
2025-02-01 23:38:02 +0200

    QFileInfo: add isOther()
    "Special" is interpreted similar to the logic used in
    QDirListing::IteratorFlag::ExcludeOther. This also matches how
    std::filesystem::is_other() is supposed to work except that in Qt .lnk
    files on Windows are treated as "other".
    
    [ChangeLog][QtCore][QFileInfo] Added isOther() method.
    
    Fixes: QTBUG-133331
    Change-Id: I2550fd37cd495b4593cacf704ce63da7d1c2addd
    Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

--- src/corelib/io/qfileinfo.h
+++ src/corelib/io/qfileinfo.h
@@ -125,6 +125,7 @@ public:
     bool isDir() const;
     bool isSymLink() const;
     bool isSymbolicLink() const;
+    bool isOther() const;
     bool isShortcut() const;
     bool isAlias() const;
     bool isJunction() const;
