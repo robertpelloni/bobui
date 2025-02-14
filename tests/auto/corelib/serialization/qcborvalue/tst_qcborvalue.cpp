qt/qtbase

commit 8e5ce9cd369230256045864d6fad38dbd8bee413

CBOR: implement faster comparison for equality in strings
If we don't need to sort, then we can use QtPrivate::equalStrings() to
get a few cycles of performance per string comparison.

Change-Id: I5f663c2f9f4149af84fefffd17c03d9b52ca696e
Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

src/corelib/serialization/qcborvalue_p.h
@@ -360,9 +360,11 @@ public:
         if (!b)
             return s.isEmpty() ? 0 : -1;
 
-        Q_UNUSED(mode);
-        if (e.flags & QtCbor::Element::StringIsUtf16)
+        if (e.flags & QtCbor::Element::StringIsUtf16) {
+            if (mode == QtCbor::Comparison::ForEquality)
+                return QtPrivate::equalStrings(b->asStringView(), s) ? 0 : 1;
             return QtPrivate::compareStrings(b->asStringView(), s);
+        }
         return compareUtf8(b, s);
     }
 

src/corelib/serialization/qcborvalue.cpp
@@ -1156,6 +1156,12 @@ static int compareElementRecursive(const QCborContainerPrivate *c1, const Elemen
     const ByteData *b1 = c1 ? c1->byteData(e1) : nullptr;
     const ByteData *b2 = c2 ? c2->byteData(e2) : nullptr;
     if (b1 || b2) {
+        auto compareStrings = [mode](auto s1, auto s2) {
+            if (mode == Comparison::ForEquality)
+                return QtPrivate::equalStrings(s1, s2) ? 0 : 1;
+            return QtPrivate::compareStrings(s1, s2);
+        };
+
         auto len1 = b1 ? b1->len : 0;
         auto len2 = b2 ? b2->len : 0;
 
@@ -1184,15 +1190,20 @@ static int compareElementRecursive(const QCborContainerPrivate *c1, const Elemen
             // Case 1: both UTF-16, so lengths are comparable.
             // (we can't use memcmp in little-endian machines)
             if (len1 == len2)
-                return QtPrivate::compareStrings(b1->asStringView(), b2->asStringView());
+                return compareStrings(b1->asStringView(), b2->asStringView());
             return len1 < len2 ? -1 : 1;
         }
 
         if (!(e1.flags & Element::StringIsUtf16) && !(e2.flags & Element::StringIsUtf16)) {
             // Cases 4, 5 and 6: neither is UTF-16, so lengths are comparable too
             // (this case includes byte arrays too)
-            if (len1 == len2)
+            if (len1 == len2) {
+                if (mode == Comparison::ForEquality) {
+                    // GCC optimizes this to __memcmpeq(); Clang to bcmp()
+                    return memcmp(b1->byte(), b2->byte(), size_t(len1)) == 0 ? 0 : 1;
+                }
                 return memcmp(b1->byte(), b2->byte(), size_t(len1));
+            }
             return len1 < len2 ? -1 : 1;
         }
 
@@ -1217,8 +1228,8 @@ static int compareElementRecursive(const QCborContainerPrivate *c1, const Elemen
         if (len1 != len2)
             return len1 < len2 ? -1 : 1;
         if (e1.flags & Element::StringIsUtf16)
-            return QtPrivate::compareStrings(b1->asStringView(), b2->asLatin1());
-        return QtPrivate::compareStrings(b1->asLatin1(), b2->asStringView());
+            return compareStrings(b1->asStringView(), b2->asLatin1());
+        return compareStrings(b1->asLatin1(), b2->asStringView());
     }
 
     return compareElementNoData(e1, e2);

robertpelloni/bobui

commit 54daec43a041cb69cff31cbfd1dd0b7127e8ba87

CBOR/JSON: fix crash when comparing strings with different length
Amends 8e5ce9cd369230256045864d6fad38dbd8bee413, which introduced the
QtPrivate::equalStrings() call. At that time, equalStrings() had already
required equal lengths (see 1560e0161af70b5cf88a70e55c0b502612d433cd),
so no excuse.

[ChangeLog][QtCore][QCborMap and QJsonObject] Fixed bug that could
result in a crash or failing to find a entry in the map/object with non-
ASCII keys.

Pick-to: 6.9 6.8
Fixes: QTBUG-133744
Change-Id: I6b0f8b0a2e47d3ef905afffda6c4c079814a0914
Reviewed-by: Marc Mutz <marc.mutz@qt.io>

src/corelib/serialization/qcborvalue_p.h
@@ -386,8 +386,8 @@ public:
 
         if (e.flags & QtCbor::Element::StringIsUtf16) {
             if (mode == QtCbor::Comparison::ForEquality)
-                return QtPrivate::equalStrings(b->asStringView(), s) ? 0 : 1;
-            return QtPrivate::compareStrings(b->asStringView(), s);
+                return b->asStringView() == s ? 0 : 1;
+            return b->asStringView().compare(s);
         }
         return compareUtf8(b, s);
     }

tests/auto/corelib/serialization/json/tst_qtjson.cpp
@@ -41,6 +41,7 @@ private Q_SLOTS:
     void testObjectTakeDetach();
     void testObjectSmallKeys();
     void testObjectInsertCopies();
+    void testObjectInsertNonAscii();
     void testArraySimple();
     void testArrayInsertCopies();
     void testValueObject();
@@ -730,6 +731,22 @@ void tst_QtJson::testObjectInsertCopies()
         QCOMPARE(obj.value("prop2"), "TEST");
         QCOMPARE(obj2.size(), 1);
         QCOMPARE(obj2.value("value"), "TEST");
+    }
+}
+
+void tst_QtJson::testObjectInsertNonAscii()
+{
+    {
+        QJsonObject myObject;
+        myObject.insert("k♭", "First key");
+        myObject.insert("a", "Second key");
+        QCOMPARE(myObject.begin().keyView(), "a");
+    }
+    {
+        QJsonObject myObject;
+        myObject.insert("a", "Second key");
+        myObject.insert("k♭", "First key");
+        QCOMPARE(myObject.begin().keyView(), "a");
     }
 }
 

tests/auto/corelib/serialization/qcborvalue/tst_qcborvalue.cpp
@@ -74,6 +74,7 @@ private slots:
     void mapMutateWithCopies();
     void mapStringValues();
     void mapStringKeys();
+    void mapStringKeysNonAscii();
     void mapValueRef_data() { basics_data(); }
     void mapValueRef();
     void mapInsertRemove_data() { basics_data(); }
@@ -1529,6 +1530,47 @@ void tst_QCborValue::mapStringKeys()
     QVERIFY(m2.value(QCborValue(QByteArray("foo"))).isUndefined());
     QVERIFY(m.value(QCborValue(QLatin1String("foo"))).isUndefined());
     QCOMPARE(m.value(QCborValue(QByteArray("foo"))).toString(), "bar");
+
+    m.insert(u"World"_s, QCborValue(3));    // replaces
+    QCOMPARE(m.size(), 3);
+    QCOMPARE(m.value(u"World"_s), QCborValue(3));
+    QCOMPARE(m.value("World"_L1), QCborValue(3));
+
+    m.insert(u"Hello"_s, QCborValue(4));    // replaces (must match Latin1)
+    QCOMPARE(m.size(), 3);
+    QCOMPARE(m.value(u"Hello"_s), QCborValue(4));
+    QCOMPARE(m.value("Hello"_L1), QCborValue(4));
+}
+
+void tst_QCborValue::mapStringKeysNonAscii()
+{
+    {
+        QCborMap m;
+        m["a"_L1] = 1;          // US-ASCII: 1 UTF-8 & UTF-16 code unit
+        m["\xE9"_L1] = 2;       // Latin-1: 2 UTF-8 code units, 1 UTF-16
+        m[u"ü"_s] = 3;          // ditto, but inserted as UTF-16
+        m[u"♭"_s] = 4;          // BMP over U+07FF: 3 UTF-8 code units, 1 UTF-16
+        m[u"\U00010000"_s] = 5; // non-BMP: 4 UTF-8 code units, 2 UTF-16
+
+        QCOMPARE(m.size(), 5);
+        QCOMPARE(m.value(u"a"_s), 1);
+        QCOMPARE(m.value(u"é"_s), 2);
+        QCOMPARE(m.value("\xFC"_L1), 3);
+
+        QCOMPARE(m.value(u"k♭"_s), QCborValue());
+        QCOMPARE(m.value("foo"_L1), QCborValue());
+    }
+    {
+        QCborMap m;
+        m[u"k♭"_s] = 1;
+        m[u"a"_s] = 2;
+        m[u"\U00010000"_s] = 3;
+
+        QCOMPARE(m.size(), 3);
+        QCOMPARE(m.value(u"b"_s), QCborValue());
+        QCOMPARE(m.value(u"♭"_s), QCborValue());
+        QCOMPARE(m.value("foo"_L1), QCborValue());
+    }
 }
 
 void tst_QCborValue::mapInsertRemove()