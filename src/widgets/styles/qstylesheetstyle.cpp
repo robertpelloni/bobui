zzag/qtbase

commit a77a7c157d5cdcc88bf0a02caed2aa5ff8850d06

QStyleSheetStyle: Use qss attributes for CE_ComboBoxLabel
CE_ComboBoxLabel did not apply any attributes if the render rule has no
box. Remove the check.

Fixes: QTBUG-131761
Pick-to: 6.8
Change-Id: I603ae6c459303f0d3a5bcc9572f12acca32bc575
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

src/widgets/styles/qstylesheetstyle.cpp
@@ -4070,8 +4070,6 @@ void QStyleSheetStyle::drawControl(ControlElement ce, const QStyleOption *opt, Q
 
 #if QT_CONFIG(combobox)
     case CE_ComboBoxLabel:
-        if (!rule.hasBox())
-            break;
         if (const QStyleOptionComboBox *cb = qstyleoption_cast<const QStyleOptionComboBox *>(opt)) {
             QRect editRect = subControlRect(CC_ComboBox, cb, SC_ComboBoxEditField, w);
             p->save();

robertpelloni/bobui

commit db07a0387497cc56d4c3a82bbbc2150ec19d2e96

StyleSheetStyle: Don't paint CE_ComboBoxLabel when no stylesheet is set
Don't paint through stylesheet code when a combobox label has no
stylesheet set at all.
This should be done also for others but may break existing painting
stuff so only fix this here as it's a regression from
a77a7c157d5cdcc88bf0a02caed2aa5ff8850d06.

Pick-to: 6.10 6.9 6.8
Task-number: QTBUG-131761
Fixes: QTBUG-137108
Change-Id: I462ba198821692aa60e6869df9c8e92332422c44
Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>

src/widgets/styles/qstylesheetstyle.cpp
@@ -4067,6 +4067,8 @@ void QStyleSheetStyle::drawControl(ControlElement ce, const QStyleOption *opt, Q
 
 #if QT_CONFIG(combobox)
     case CE_ComboBoxLabel:
+        if (!rule.hasDrawable())
+            break;
         if (const QStyleOptionComboBox *cb = qstyleoption_cast<const QStyleOptionComboBox *>(opt)) {
             QRect editRect = subControlRect(CC_ComboBox, cb, SC_ComboBoxEditField, w);
             p->save();