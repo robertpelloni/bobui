06b5d2e7abc2ad74d373bd4a800fa72512a298d3
Alexey Edelev <alexey.edelev@qt.io>
2025-07-09 17:41:53 +0200

    Check android bundles in test_android_signing
    Extend the 'test_android_signing' test so it now supports checking both
    AAB and APK outputs.
    
    Change-Id: Ia0dfd09a240691f65c78658a572527cd4ab94119
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/auto/cmake/test_android_signing/verify_signature.cmake
+++ tests/auto/cmake/test_android_signing/verify_signature.cmake
@@ -5,21 +5,22 @@ if(NOT KEYTOOL_PATH)
     message(FATAL_ERROR "KEYTOOL_PATH is missing.")
 endif()
 
-if(NOT APK_FILE)
-    message(FATAL_ERROR "APK_FILE is missing.")
+if(NOT OUTPUT_FILE)
+    message(FATAL_ERROR "OUTPUT_FILE is missing.")
 endif()
 
 execute_process(
-    COMMAND ${KEYTOOL_PATH} -printcert -jarfile ${APK_FILE}
+    COMMAND ${KEYTOOL_PATH} -printcert -jarfile ${OUTPUT_FILE}
     RESULT_VARIABLE keytool_result
     OUTPUT_VARIABLE keytool_output
     ERROR_VARIABLE keytool_error
 )
 
 if(NOT keytool_result EQUAL 0)
-    message(FATAL_ERROR "Keytool command failed with error: ${keytool_error}")
+    message(FATAL_ERROR "Keytool command '${KEYTOOL_PATH}' -printcert -jarfile '${OUTPUT_FILE}'"
+        "failed with error: ${keytool_error} ${keytool_output}")
 endif()
 
 if(NOT keytool_output MATCHES ".+Certificate #1:.+")
-    message(FATAL_ERROR "APK is not signed.")
+    message(FATAL_ERROR "Package ${OUTPUT_FILE} is not signed.")
 endif()
