a74a1ee297ad4540d9b28e2cdeebe02eafe3c3a7
Oleksii Zbykovskyi <Oleksii.Zbykovskyi@qt.io>
2025-07-25 09:54:49 +0200

    Fix of files under mimetype and plugin prefix in code folder
    Made these files compileable.
    
    Task-number: QTBUG-137566
    Change-Id: I68747552d4508ebee1f9fbd62683a3738ef9050f
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/corelib/doc/snippets/code/src_corelib_mimetype_qmimedatabase.cpp
+++ src/corelib/doc/snippets/code/src_corelib_mimetype_qmimedatabase.cpp
@@ -1,23 +1,31 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
+#include <QMimeDatabase>
+#include <QMimeType>
+#include <QStandardPaths>
+
 using namespace Qt::StringLiterals;
 
-//! [0]
-QMimeDatabase db;
-QMimeType mime = db.mimeTypeForFile(fileName);
-if (mime.inherits("text/plain")) {
-    // The file is plain text, we can display it in a QTextEdit
+void examples(QString fileName)
+{
+    //! [0]
+    QMimeDatabase db;
+    QMimeType mime = db.mimeTypeForFile(fileName);
+    if (mime.inherits("text/plain")) {
+        // The file is plain text, we can display it in a QTextEdit
+    }
+    //! [0]
+
+    //! [1]
+    using namespace Qt::StringLiterals;
+    // ...
+    QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, "mime/packages"_L1,
+                              QStandardPaths::LocateDirectory);
+    //! [1]
 }
-//! [0]
 
-//! [1]
-using namespace Qt::StringLiterals;
-// ...
-QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, "mime/packages"_L1,
-                          QStandardPaths::LocateDirectory);
-//! [1]
-
+#if 0
 //! [2]
 <?xml version="1.0" encoding="UTF-8"?>
 <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
@@ -27,3 +35,4 @@ QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, "mime/packages"_L
   </mime-type>
 </mime-info>
 //! [2]
+#endif
--- src/corelib/doc/snippets/code/src_corelib_plugin_qlibrary.cpp
+++ src/corelib/doc/snippets/code/src_corelib_plugin_qlibrary.cpp
@@ -1,42 +1,42 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-//! [0]
-QLibrary myLib("mylib");
-typedef void (*MyPrototype)();
-MyPrototype myFunction = (MyPrototype) myLib.resolve("mysymbol");
-if (myFunction)
-    myFunction();
-//! [0]
+#include <QLibrary>
 
-
-//! [1]
-typedef void (*MyPrototype)();
-MyPrototype myFunction =
-        (MyPrototype) QLibrary::resolve("mylib", "mysymbol");
-if (myFunction)
-    myFunction();
-//! [1]
-
-
-//! [2]
-typedef int (*AvgFunction)(int, int);
-
-AvgFunction avg = (AvgFunction) library->resolve("avg");
-if (avg)
-    return avg(5, 8);
-else
-    return -1;
-//! [2]
-
-
-//! [3]
-extern "C" MY_EXPORT int avg(int a, int b)
+int examples(QLibrary *library)
 {
-    return (a + b) / 2;
-}
-//! [3]
+    {
+        //! [0]
+        QLibrary myLib("mylib");
+        typedef void (*MyPrototype)();
+        MyPrototype myFunction = (MyPrototype) myLib.resolve("mysymbol");
+        if (myFunction)
+            myFunction();
+        //! [0]
+    }
 
+    {
+        //! [1]
+        typedef void (*MyPrototype)();
+        MyPrototype myFunction =
+                (MyPrototype) QLibrary::resolve("mylib", "mysymbol");
+        if (myFunction)
+            myFunction();
+        //! [1]
+    }
+
+    {
+        //! [2]
+        typedef int (*AvgFunction)(int, int);
+
+        AvgFunction avg = (AvgFunction) library->resolve("avg");
+        if (avg)
+            return avg(5, 8);
+        else
+            return -1;
+        //! [2]
+    }
+}
 
 //! [4]
 #ifdef Q_OS_WIN
@@ -45,3 +45,10 @@ extern "C" MY_EXPORT int avg(int a, int b)
 #define MY_EXPORT
 #endif
 //! [4]
+
+//! [3]
+extern "C" MY_EXPORT int avg(int a, int b)
+{
+    return (a + b) / 2;
+}
+//! [3]
