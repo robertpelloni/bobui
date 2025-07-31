qt/qtbase

commit b211148e4b4167483e1a78a45ae8fef97e5a4f2d

Sql ODBC driver: add direct support for float and short datatype
This patch adds native support for SQL_REAL (float) and SQL_SMALLINT
(short). Previously those datatypes were mapped to double and integer.

[ChangeLog][QtSql] The ODBC driver now properly maps QMetaType::Float to
real sql datatype and QMetaType::Short to smallint

Fixes: QTBUG-8963
Fixes: QTBUG-57279
Change-Id: Ifec4c609734dbe6165c1ebdadb461c2aae47ba78
Reviewed-by: Andy Shaw <andy.shaw@qt.io>

src/plugins/sqldrivers/odbc/qsql_odbc.cpp
@@ -347,12 +347,16 @@ static QMetaType qDecodeODBCType(SQLSMALLINT sqltype, bool isSigned = true)
     switch (sqltype) {
     case SQL_DECIMAL:
     case SQL_NUMERIC:
-    case SQL_REAL:
-    case SQL_FLOAT:
-    case SQL_DOUBLE:
+    case SQL_FLOAT: // 24 or 53 bits precision
+    case SQL_DOUBLE:// 53 bits
         type = QMetaType::Double;
         break;
+    case SQL_REAL:  // 24 bits
+        type = QMetaType::Float;
+        break;
     case SQL_SMALLINT:
+        type = isSigned ? QMetaType::Short : QMetaType::UShort;
+        break;
     case SQL_INTEGER:
     case SQL_BIT:
         type = isSigned ? QMetaType::Int : QMetaType::UInt;
@@ -1220,9 +1224,11 @@ QVariant QODBCResult::data(int field)
             d->fieldCache[i] = qGetBigIntData(d->hStmt, i, false);
             break;
         case QMetaType::Int:
+        case QMetaType::Short:
             d->fieldCache[i] = qGetIntData(d->hStmt, i);
-        break;
+            break;
         case QMetaType::UInt:
+        case QMetaType::UShort:
             d->fieldCache[i] = qGetIntData(d->hStmt, i, false);
             break;
         case QMetaType::QDate:
@@ -1513,12 +1519,48 @@ bool QODBCResult::exec()
                                       0,
                                       *ind == SQL_NULL_DATA ? ind : NULL);
                 break;
+            case QMetaType::Short:
+                r = SQLBindParameter(d->hStmt,
+                                      i + 1,
+                                      qParamType[bindValueType(i) & QSql::InOut],
+                                      SQL_C_SSHORT,
+                                      SQL_SMALLINT,
+                                      0,
+                                      0,
+                                      const_cast<void *>(val.constData()),
+                                      0,
+                                      *ind == SQL_NULL_DATA ? ind : NULL);
+                break;
+            case QMetaType::UShort:
+                r = SQLBindParameter(d->hStmt,
+                                      i + 1,
+                                      qParamType[bindValueType(i) & QSql::InOut],
+                                      SQL_C_USHORT,
+                                      SQL_NUMERIC,
+                                      15,
+                                      0,
+                                      const_cast<void *>(val.constData()),
+                                      0,
+                                      *ind == SQL_NULL_DATA ? ind : NULL);
+                break;
             case QMetaType::Double:
                 r = SQLBindParameter(d->hStmt,
                                       i + 1,
                                       qParamType[bindValueType(i) & QSql::InOut],
                                       SQL_C_DOUBLE,
                                       SQL_DOUBLE,
+                                      0,
+                                      0,
+                                      const_cast<void *>(val.constData()),
+                                      0,
+                                      *ind == SQL_NULL_DATA ? ind : NULL);
+                break;
+            case QMetaType::Float:
+                r = SQLBindParameter(d->hStmt,
+                                      i + 1,
+                                      qParamType[bindValueType(i) & QSql::InOut],
+                                      SQL_C_FLOAT,
+                                      SQL_REAL,
                                       0,
                                       0,
                                       const_cast<void *>(val.constData()),
@@ -1705,8 +1747,11 @@ bool QODBCResult::exec()
                                QTime(dt.hour, dt.minute, dt.second, dt.fraction / 1000000)));
                 break; }
             case QMetaType::Bool:
+            case QMetaType::Short:
+            case QMetaType::UShort:
             case QMetaType::Int:
             case QMetaType::UInt:
+            case QMetaType::Float:
             case QMetaType::Double:
             case QMetaType::QByteArray:
             case QMetaType::LongLong:

tests/auto/sql/kernel/qsqldatabase/tst_qsqldatabase.cpp
@@ -99,6 +99,8 @@ private slots:
     void recordSQLServer();
     void recordIBase_data() {generic_data("QIBASE"); }
     void recordIBase();
+    void recordODBC_data() { generic_data("QODBC"); }
+    void recordODBC();
 
     void eventNotificationIBase_data() { generic_data("QIBASE"); }
     void eventNotificationIBase();
@@ -1006,6 +1008,41 @@ void tst_QSqlDatabase::recordIBase()
         FieldDef("decimal(18)", QMetaType::LongLong, Q_INT64_C(9223372036854775807)),
         FieldDef("numeric(5,2)", QMetaType::Double, 123.45),
         FieldDef("boolean", QMetaType::Bool, true),
+
+        FieldDef()
+    };
+
+    const int fieldCount = createFieldTable(fieldDefs, db);
+    QVERIFY(fieldCount > 0);
+
+    commonFieldTest(fieldDefs, db, fieldCount);
+}
+
+void tst_QSqlDatabase::recordODBC()
+{
+    QFETCH(QString, dbName);
+    QSqlDatabase db = QSqlDatabase::database(dbName);
+    CHECK_DATABASE(db);
+
+    static QDateTime dt(QDate::currentDate(), QTime(1, 2, 3, 0));
+    static const FieldDef fieldDefs[] = {
+        FieldDef("decimal(10, 9)", QMetaType::Double,     1.123456789),
+        FieldDef("numeric(5, 2)", QMetaType::Double,      123.67),
+        FieldDef("float", QMetaType::Double,              1.123456789),
+        FieldDef("double precision", QMetaType::Double,   1.123456789),
+        FieldDef("real", QMetaType::Float,                1.12345),
+
+//        FieldDef("tinyint", QMetaType::Char, 127),    // not supported by psqlODBC
+        FieldDef("smallint", QMetaType::Short,            32767),
+        FieldDef("integer", QMetaType::Int,               2147483647),
+        FieldDef("bigint", QMetaType::LongLong,           Q_INT64_C(9223372036854775807)),
+
+        FieldDef("date", QMetaType::QDate,                 QDate::currentDate()),
+        FieldDef("timestamp", QMetaType::QDateTime,        dt, false),
+        FieldDef("time", QMetaType::QTime,                 dt.time()),
+        FieldDef("char(20)", QMetaType::QString,           "Blah"),
+        FieldDef("varchar(20)", QMetaType::QString,        "BlahBlah"),
+        FieldDef("text", QMetaType::QString,               QString("blah6")),
 
         FieldDef()
     };

robertpelloni/bobui

commit 7f24d895214a806270acb83c2227e3fc0380d978

SQL/ODBC: fix reading REAL values
Reading out data of type REAL was broken since
b211148e4b4167483e1a78a45ae8fef97e5a4f2d due to the fact that
QMetaType::Float was not handled within QODBCResult::data().
It was also not possible to read out the correct data with the help of
QSql::HighPrecision due to a wrong length given to qGetStringData().

Pick-to: 6.10 6.9 6.8
Fixes: QTBUG-138642
Task-number: QTBUG-8963
Change-Id: I90a003a0b1625f37931468c3994418a1dc3a03c4
Reviewed-by: Andy Shaw <andy.shaw@qt.io>

src/plugins/sqldrivers/odbc/qsql_odbc.cpp
@@ -544,22 +544,21 @@ static QVariant qGetIntData(SQLHANDLE hStmt, int column)
     return QVariant::fromValue<T>(intbuf);
 }
 
-static QVariant qGetDoubleData(SQLHANDLE hStmt, int column)
+template <typename T>
+static QVariant qGetFloatingPointData(SQLHANDLE hStmt, int column)
 {
-    SQLDOUBLE dblbuf = 0;
+    constexpr auto tgtType = sizeof(T) == 4 ? SQL_C_FLOAT : SQL_C_DOUBLE;
+    static_assert(std::is_same_v<T, float> || std::is_same_v<T, double>,
+                  "Can only handle float or double");
+    T buffer = 0;
     SQLLEN lengthIndicator = 0;
-    SQLRETURN r = SQLGetData(hStmt,
-                              column+1,
-                              SQL_C_DOUBLE,
-                              (SQLPOINTER) &dblbuf,
-                              0,
-                              &lengthIndicator);
+    SQLRETURN r = SQLGetData(hStmt, column + 1, tgtType,
+                             (SQLPOINTER)&buffer, sizeof(buffer), &lengthIndicator);
     if (!SQL_SUCCEEDED(r))
         return QVariant();
     if (lengthIndicator == SQL_NULL_DATA)
-        return QVariant(QMetaType::fromType<double>());
-
-    return (double) dblbuf;
+        return QVariant(QMetaType::fromType<T>());
+    return QVariant::fromValue<T>(buffer);
 }
 
 static bool isAutoValue(const SQLHANDLE hStmt, int column)
@@ -1149,7 +1148,8 @@ QVariant QODBCResult::data(int field)
         // some servers do not support fetching column n after we already
         // fetched column n+1, so cache all previous columns here
         const QSqlField info = d->rInf.field(i);
-        switch (info.metaType().id()) {
+        const auto metaTypeId = info.metaType().id();
+        switch (metaTypeId) {
         case QMetaType::LongLong:
             d->fieldCache[i] = qGetIntData<int64_t>(d->hStmt, i);
             break;
@@ -1214,8 +1214,9 @@ QVariant QODBCResult::data(int field)
         case QMetaType::QString:
             d->fieldCache[i] = qGetStringData(d->hStmt, i, info.length(), d->unicode);
             break;
+        case QMetaType::Float:
         case QMetaType::Double:
-            switch(numericalPrecisionPolicy()) {
+            switch (numericalPrecisionPolicy()) {
                 case QSql::LowPrecisionInt32:
                     d->fieldCache[i] = qGetIntData<int32_t>(d->hStmt, i);
                     break;
@@ -1223,11 +1224,13 @@ QVariant QODBCResult::data(int field)
                     d->fieldCache[i] = qGetIntData<int64_t>(d->hStmt, i);
                     break;
                 case QSql::LowPrecisionDouble:
-                    d->fieldCache[i] = qGetDoubleData(d->hStmt, i);
+                    if (metaTypeId == QMetaType::Float)
+                        d->fieldCache[i] = qGetFloatingPointData<float>(d->hStmt, i);
+                    else
+                        d->fieldCache[i] = qGetFloatingPointData<double>(d->hStmt, i);
                     break;
                 case QSql::HighPrecision:
-                    const int extra = info.precision() > 0 ? 1 : 0;
-                    d->fieldCache[i] = qGetStringData(d->hStmt, i, info.length() + extra, false);
+                    d->fieldCache[i] = qGetStringData(d->hStmt, i, -1, false);
                     break;
             }
             break;

tests/auto/sql/kernel/qsqlquery/tst_qsqlquery.cpp
@@ -243,6 +243,9 @@ private slots:
     void QTBUG_73286_data() { generic_data("QODBC"); }
     void QTBUG_73286();
 
+    void ODBC_FloatingPoint_data() { return generic_data("QODBC"); }
+    void ODBC_FloatingPoint();
+
     void insertVarChar1_data() { generic_data("QODBC"); }
     void insertVarChar1();
 
@@ -4411,7 +4414,7 @@ void tst_QSqlQuery::aggregateFunctionTypes()
         const auto &tableName = ts.tableName();
 
         QSqlQuery q(db);
-        QVERIFY_SQL(q, exec(QLatin1String("CREATE TABLE %1 (id REAL)").arg(tableName)));
+        QVERIFY_SQL(q, exec(QLatin1String("CREATE TABLE %1 (id DOUBLE PRECISION)").arg(tableName)));
 
         // First test without any entries
         QVERIFY_SQL(q, exec("SELECT SUM(id) FROM " + tableName));
@@ -4685,6 +4688,34 @@ void tst_QSqlQuery::QTBUG_73286()
     QCOMPARE(q.value(1).toString(), "12345678901234567890");
     QCOMPARE(q.value(2).toString(), "12345678901234567.890");
 }
+
+void tst_QSqlQuery::ODBC_FloatingPoint()
+{
+    QFETCH(QString, dbName);
+    QSqlDatabase db = QSqlDatabase::database(dbName);
+    CHECK_DATABASE(db);
+    TableScope ts(db, "qtbug138642", __FILE__);
+
+    constexpr auto val1 = 0.015625; // more than 7 chars which is the length of real!
+    constexpr auto val2 = 0.09375;
+    QSqlQuery q(db);
+    QVERIFY_SQL(q, exec("CREATE TABLE %1 (Id INT, Column1 REAL, Column2 FLOAT)"_L1.arg(ts.tableName())));
+    QVERIFY_SQL(q, exec("INSERT INTO %1 (Id, Column1, Column2) Values (1, %2, %2)"_L1.arg(ts.tableName()).arg(val1)));
+    QVERIFY_SQL(q, exec("INSERT INTO %1 (Id, Column1, Column2) Values (2, %2, %2)"_L1.arg(ts.tableName()).arg(val2)));
+
+    const auto prec = {QSql::HighPrecision, QSql::LowPrecisionDouble};
+    for (const auto p  : prec) {
+        q.setNumericalPrecisionPolicy(p);
+        QVERIFY_SQL(q, exec("SELECT Column1, Column2 FROM %1 ORDER BY Id"_L1.arg(ts.tableName())));
+        QVERIFY_SQL(q, next());
+        QCOMPARE(q.value(0).toDouble(), val1);
+        QCOMPARE(q.value(1).toDouble(), val1);
+        QVERIFY_SQL(q, next());
+        QCOMPARE(q.value(0).toDouble(), val2);
+        QCOMPARE(q.value(1).toDouble(), val2);
+    }
+}
+
 
 void tst_QSqlQuery::insertVarChar1()
 {