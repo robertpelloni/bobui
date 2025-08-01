robertpelloni/bobui

commit a318e331f1387eb3c9d13be96c28619453a35571

QUnifiedTimer: allow speeding up animations and simplify API
Speeding up animations in tst_qquickpopup by 5 times results in a
reduction of its execution time from 9.8 to 7.9 minutes: a 20%
improvement.

This patch:
- Renames setSlowdownFactor to setSpeedModifier.
- Removes setSlowModeEnabled. There's no need for two functions if
  we can just set a speed directly.

Task-number: QTBUG-137919
Change-Id: I04eb5d962c818da5d36f8f4cb5a2404977b78deb
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/animation/qabstractanimation.cpp
@@ -179,9 +179,9 @@ typedef QList<QAbstractAnimation*>::ConstIterator AnimationListConstIt;
 
 QUnifiedTimer::QUnifiedTimer() :
     QObject(), defaultDriver(this), lastTick(0), timingInterval(DEFAULT_TIMER_INTERVAL),
-    currentAnimationIdx(0), insideTick(false), insideRestart(false), consistentTiming(false), slowMode(false),
+    currentAnimationIdx(0), insideTick(false), insideRestart(false), consistentTiming(false),
     startTimersPending(false), stopTimerPending(false), allowNegativeDelta(false),
-    slowdownFactor(5.0f), profilerCallback(nullptr),
+    speedModifier(1), profilerCallback(nullptr),
     driverStartTime(0), temporalDrift(0)
 {
     time.invalidate();
@@ -265,9 +265,11 @@ void QUnifiedTimer::updateAnimationTimers()
     // ignore consistentTiming in case the pause timer is active
     qint64 delta = (consistentTiming && !pauseTimer.isActive()) ?
                         timingInterval : totalElapsed - lastTick;
-    if (slowMode) {
-        if (slowdownFactor > 0)
-            delta = qRound(delta / slowdownFactor);
+    // Don't use qFuzzyCompare because this won't be set frequently enough
+    // that floating point error can accumulate.
+    if (speedModifier != 1) {
+        if (speedModifier > 0)
+            delta = qRound(delta / speedModifier);
         else
             delta = 0;
     }

src/corelib/animation/qabstractanimation_p.h
@@ -150,9 +150,9 @@ public:
     */
     void setConsistentTiming(bool consistent) { consistentTiming = consistent; }
 
-    //these facilitate fine-tuning of complex animations
-    void setSlowModeEnabled(bool enabled) { slowMode = enabled; }
-    void setSlowdownFactor(qreal factor) { slowdownFactor = factor; }
+    // This facilitates both fine-tuning of complex animations by slowing them
+    // them down, and reducing execution time of auto tests by speeding them up.
+    void setSpeedModifier(qreal speed) { speedModifier = speed; }
 
     void installAnimationDriver(QAnimationDriver *driver);
     void uninstallAnimationDriver(QAnimationDriver *driver);
@@ -194,15 +194,13 @@ private:
     bool insideTick;
     bool insideRestart;
     bool consistentTiming;
-    bool slowMode;
     bool startTimersPending;
     bool stopTimerPending;
     bool allowNegativeDelta;
 
-    // This factor will be used to divide the DEFAULT_TIMER_INTERVAL at each tick
-    // when slowMode is enabled. Setting it to 0 or higher than DEFAULT_TIMER_INTERVAL (16)
-    // stops all animations.
-    qreal slowdownFactor;
+    // This factor will be used to multiply the DEFAULT_TIMER_INTERVAL (16) at each tick
+    // if it's not equal to 1. Setting it to less than 1 / 16 (0.0625) stops all animations.
+    qreal speedModifier;
 
     QList<QAbstractAnimationTimer*> animationTimers, animationTimersToStart;
     QList<QAbstractAnimationTimer*> pausedAnimationTimers;

commit 10799a36a05862faef71753d8627d376a7a3efb6

QUnifiedTimer: fix speed-modified delta calculation
a318e331f1387eb3c9d13be96c28619453a35571 accidentally divided when it
should have been multiplication. E.g the old code would actually
slow animations down instead of speeding them up:

    delta (16) / speedModifier (5) = 3.2

What we want is:

    16 * 5 = 80

Add an auto test for the behavior in
tst_QPropertyAnimation::speedModifier. So that we can restore
speedModifier's previous value, add a getter for it.

Task-number: QTBUG-137919
Change-Id: Ie873a5c90e36bb9906aadad45fa2c53f50066c1e
Reviewed-by: Matthias Rauter <matthias.rauter@qt.io>
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

src/corelib/animation/qabstractanimation.cpp
@@ -275,7 +275,7 @@ void QUnifiedTimer::updateAnimationTimers()
     // that floating point error can accumulate.
     if (speedModifier != 1) {
         if (speedModifier > 0)
-            delta = qRound(delta / speedModifier);
+            delta = qRound(delta * speedModifier);
         else
             delta = 0;
     }

src/corelib/animation/qabstractanimation_p.h
@@ -153,6 +153,7 @@ public:
 
     // This facilitates both fine-tuning of complex animations by slowing them
     // them down, and reducing execution time of auto tests by speeding them up.
+    qreal getSpeedModifier() const { return speedModifier; }
     void setSpeedModifier(qreal speed) { speedModifier = speed; }
 
     void installAnimationDriver(QAnimationDriver *driver);

tests/auto/corelib/animation/qpropertyanimation/tst_qpropertyanimation.cpp
@@ -13,6 +13,8 @@
 #include <QtGui/qpointingdevice.h>
 #include <QtWidgets/qwidget.h>
 
+#include <math.h>
+
 Q_DECLARE_METATYPE(QAbstractAnimation::State)
 
 class UncontrolledAnimation : public QPropertyAnimation
@@ -171,6 +173,8 @@ private slots:
     void zeroLoopCount();
     void recursiveAnimations();
     void bindings();
+    void speedModifier_data();
+    void speedModifier();
 };
 
 void tst_QPropertyAnimation::initTestCase()
@@ -1402,6 +1406,102 @@ void tst_QPropertyAnimation::bindings()
     a.setTargetObject(o1.get());
     o1.reset();
     QCOMPARE(a.targetObject(), nullptr);
+}
+
+void tst_QPropertyAnimation::speedModifier_data()
+{
+    QTest::addColumn<int>("duration");
+    QTest::addColumn<qreal>("speedModifier");
+    QTest::addColumn<int>("waitDuration");
+
+    // It should progress at 50% of the ordinary animation speed. If the speedModifier
+    // was 1, it would be 0.5 after 200 milliseconds, so we expect (via the calculation for
+    // "expected") half of that.
+    QTest::newRow("0.5") << 400 << 0.5 << 200;
+
+    // It should progress at 200% of the ordinary animation speed, so should
+    // have already finished after 200 milliseconds.
+    QTest::newRow("2") << 400 << 2.0 << 200;
+}
+
+void tst_QPropertyAnimation::speedModifier()
+{
+    QFETCH(int, duration);
+    QFETCH(qreal, speedModifier);
+    QFETCH(int, waitDuration);
+
+    const qreal previousSpeedModifier = QUnifiedTimer::instance()->getSpeedModifier();
+    QUnifiedTimer::instance()->setSpeedModifier(speedModifier);
+    auto cleanup = qScopeGuard([previousSpeedModifier](){
+        QUnifiedTimer::instance()->setSpeedModifier(previousSpeedModifier);
+    });
+
+    MyObject targetObject;
+    QPropertyAnimation animation(&targetObject, "x");
+    animation.setStartValue(0.0);
+    animation.setEndValue(1.0);
+    animation.setDuration(duration);
+    animation.start();
+    QCOMPARE(animation.state(), QAbstractAnimation::Running);
+
+    constexpr qreal tiny = 0.025, close = 0.05, adequate = 0.5;
+    const qreal target = waitDuration * speedModifier / duration;
+    Q_ASSERT(target <= 1.0); // else: bad test-case, needs shorter waitDuration.
+    // How long does it take us to get within tiny of that target?
+    QElapsedTimer timer;
+    timer.start();
+    QTRY_VERIFY(targetObject.x() + tiny >= target);
+
+    // How much of the animation did that actually play ?
+    const qreal fractionPlayed = targetObject.x();
+    // How long did it in fact take ?
+    const qint64 nsecsTaken = timer.nsecsElapsed();
+    // Convert to float millis (use integer div/mod to retain ns precision):
+    const qreal msecsTaken
+        = double(nsecsTaken / 2048) * 2.048e-3 + double(nsecsTaken % 2048) * 1e-6;
+    // qint64 has 63 bits of precision; / 2048 cuts that to 52 that double
+    // faithfully represents; * 2.048e-3 then converts to millis. Remainder has
+    // only 11 bits, so is faithfully represented and * 1e-6 converts it to millis.
+    const qreal actualSpeedMod = qreal(duration) * fractionPlayed / msecsTaken;
+    // Ideally that'd == speedModifier, but it should at least be on the same side
+    // of 1; log has the same sign if that's true, so test the ratio of logs.
+    const qreal logExpected = log(speedModifier);
+
+    // We'll pass on a rather crude check, but produce this report unless
+    // logActual is quite close to logExpected.
+    auto report = qScopeGuard([actualSpeedMod, fractionPlayed, msecsTaken]() {
+        // Report naturally gives speedModifier as test-case name, implying durations.
+        qDebug("Actual speed modifier %.03f; completed %.03f of whole in %.1f ms",
+            actualSpeedMod, fractionPlayed, msecsTaken);
+    });
+
+    if (qAbs(logExpected) < tiny) {
+        // No test-case currently exercises this:
+        Q_ASSERT(!"Need to work out what to test in this case");
+        // We can't divide by logExpected, so need a different test
+        // for this case, if we ever add it.
+
+    } else if (fractionPlayed < 1.0 || logExpected < 0.0) {
+        const qreal logActual = log(actualSpeedMod);
+        // Same sign logs, with actual not too small compared to expected ?
+        QCOMPARE_GT(logActual / logExpected, adequate);
+        if (qAbs(logActual / logExpected - 1) < close)
+            report.dismiss();
+    } else {
+        // Animation played to completion, so msecsTaken is an upper bound;
+        // it may have finished before then, making actualSpeedMod a lower bound;
+        // it may have run faster. So accept if there's any a >= logActual for
+        // which the < 1 branch would have accepted had logActual been a.
+        // No such a would have helped for logExpected < 0, so it's handled above.
+        // For logExpected > 0, there is necessarily some a >= logActual for which
+        // a / logExpected > adequate. So all we can check, sensibly, is whether
+        // to dismiss the report.
+        // For that, we want an actual run close to what we asked for.
+        if (target >= 1.0) // matching fractionPlayed >= 1
+            report.dismiss();
+        else if (qAbs(actualSpeedMod / speedModifier - 1) < close)
+            report.dismiss();
+    }
 }
 
 QTEST_MAIN(tst_QPropertyAnimation)