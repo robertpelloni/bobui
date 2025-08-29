robertpelloni/bobui

commit 82eb0fc09e048b6074a9205e371a8c17576b421a

Q_IMPORT_PLUGIN: namespace RAII class
the generated StaticXXPluginInstance class is globally visible and not
properly namespaced.
Moving the imlementation and instantiation into an anonymous namespace.

[ChangeLog][Potentially Source-Incompatible Changes] The Q_IMPORT_PLUGIN
macro must be placed in file-scope. It cannot be placed into
function-scope any more, as it was never intended to be.

Pick-to: 6.10
Change-Id: I1f921259dfcf15f78acc5b09125b4e5deb50a83a
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/plugin/qplugin.h
@@ -191,15 +191,16 @@ public:
     }
 };
 
-#define Q_IMPORT_PLUGIN(PLUGIN) \
+#define Q_IMPORT_PLUGIN(PLUGIN)                                                                            \
         extern const QT_PREPEND_NAMESPACE(QStaticPlugin) QT_MANGLE_NAMESPACE(qt_static_plugin_##PLUGIN)(); \
-        class Static##PLUGIN##PluginInstance{ \
-        public: \
-                Static##PLUGIN##PluginInstance() { \
-                    qRegisterStaticPluginFunction(QT_MANGLE_NAMESPACE(qt_static_plugin_##PLUGIN)()); \
-                } \
-        }; \
-       static Static##PLUGIN##PluginInstance static##PLUGIN##Instance;
+        namespace {                                                                                        \
+            struct Static##PLUGIN##PluginInstance {                                                        \
+                Static##PLUGIN##PluginInstance() {                                                         \
+                    qRegisterStaticPluginFunction(QT_MANGLE_NAMESPACE(qt_static_plugin_##PLUGIN)());       \
+                }                                                                                          \
+            };                                                                                             \
+            Static##PLUGIN##PluginInstance static##PLUGIN##Instance;                                       \
+        } /* namespace */
 
 #if defined(QT_PLUGIN_RESOURCE_INIT_FUNCTION)
 #  define QT_PLUGIN_RESOURCE_INIT \

src/corelib/plugin/qplugin.qdoc
@@ -69,8 +69,8 @@
     with the name of the class that declares metadata for the plugin with
     Q_PLUGIN_METADATA().
 
-    Inserting this macro into your application's source code will allow
-    you to make use of a static plugin.
+    Inserting this macro into your application's source code at file scope of
+    the root namespace will allow you to make use of a static plugin.
 
     Example:
 

commit 6fdfbb5163fffedcd29d6d2ca843cad5e4ae12de

Q_IMPORT_PLUGIN: work around clazy warning
clazy excludes Q_IMPORT_PLUGIN from clazy-non-pod-global-static. However
the refactoring in 82eb0fc09e048b60 broke macro detection.
We can work around this macro detection issue by declaring the RAII
class as static inside the anonymous namespace.

Fixes: QTBUG-139615
Pick-to: 6.10
Change-Id: Iab895c494423c9c21fd1bc19d3751df5891764fe
Reviewed-by: Thiago Macieira <thiago.macieira@intel.com>

src/corelib/plugin/qplugin.h
@@ -199,8 +199,9 @@ public:
                     qRegisterStaticPluginFunction(QT_MANGLE_NAMESPACE(qt_static_plugin_##PLUGIN)());       \
                 }                                                                                          \
             };                                                                                             \
-            Static##PLUGIN##PluginInstance static##PLUGIN##Instance;                                       \
-        } /* namespace */
+        } /* namespace */                                                                                  \
+        /* QTBUG-139615: static, to work around bug in clazy-non-pod-global-static */                      \
+        static Static##PLUGIN##PluginInstance static##PLUGIN##Instance;                                    \
 
 #if defined(QT_PLUGIN_RESOURCE_INIT_FUNCTION)
 #  define QT_PLUGIN_RESOURCE_INIT \