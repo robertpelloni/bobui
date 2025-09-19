35accdaa5ca142e9c34342cc579c1f1be114eb72
Marc Mutz <marc.mutz@qt.io>
2025-09-19 23:05:02 +0200

    Fix Qt-Security score:(sensitive => significant)
    We merged several markings with the wrong tag. Fix them.
    
    Amends 677f97ecad30577a7e4cc7423554e66a4f19abba,
    17e58599bed71be3b119874aa4ea296f44081977,
    993f327901cc93e5617fa0b2172aab4793d5be34,
    9e479614d95a0859491ef8cbcc79a3a92f18a649,
    5f6bfa3692a91c9f162606c25ec496656c253dfe,
    9efcf1457229ef33a4c336b6a8ffd31333f5c105, and
    8a5b5584006b21cdefd931c7f173ac546a89a100.
    
    Pick-to: 6.10.0 6.10 6.8
    Task-number: QTBUG-135195
    Change-Id: Ica405acf0915fd584c98a504f0d4bfa57dc33482
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>

--- src/corelib/text/qstringconverter_base_deprecated.h
+++ src/corelib/text/qstringconverter_base_deprecated.h
@@ -1,6 +1,6 @@
 // Copyright (C) 2025 Intel Corporation
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
-// Qt-Security score:sensitive reason:header-decls-only
+// Qt-Security score:significant reason:header-decls-only
 
 #ifndef QSTRINGCONVERTER_BASE_DEPRECATED_H
 #define QSTRINGCONVERTER_BASE_DEPRECATED_H
