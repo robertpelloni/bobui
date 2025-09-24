32591166b1feedc709d9c2b56e75370f46ab8b0c
Axel Spoerl <axel.spoerl@qt.io>
2025-09-24 20:12:15 +0200

    Remove Qt::WindowType::Desktop in QWindowsVistaStyle
    Qt::WindowType::Desktop is no longer a valid window type in Qt6.
    Remove its occurence in QWindowsVistaStyle.
    
    Task-number: QTBUG-140514
    Change-Id: I3332ce4d90094a43a0ebbd85ba00247b6d14d304
    Reviewed-by: Wladimir Leuschner <wladimir.leuschner@qt.io>

--- src/plugins/styles/modernwindows/qwindowsvistastyle.cpp
+++ src/plugins/styles/modernwindows/qwindowsvistastyle.cpp
@@ -379,7 +379,7 @@ HWND QWindowsVistaStylePrivate::winId(const QWidget *widget)
     // Find top level with native window (there might be dialogs that do not have one).
     const auto allWindows = QGuiApplication::allWindows();
     for (const QWindow *window : allWindows) {
-        if (window->isTopLevel() && window->type() != Qt::Desktop && window->handle() != nullptr)
+        if (window->isTopLevel() && window->handle() != nullptr)
             return reinterpret_cast<HWND>(window->winId());
     }
 
