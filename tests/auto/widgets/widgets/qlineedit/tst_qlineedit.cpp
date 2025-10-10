38b265eb2aa9bc3bdc8f12c7f8784dd174c339d5
Allan Sandfeld Jensen <allan.jensen@qt.io>
2025-10-10 13:55:33 +0200

    Remove expected failure for qemu-armv7
    It passes on qemu-armv7 and vxworks armv7, which is all the
    armv7 platforms we currently test. Amends
    4772ac90fa84a81292477a4c9a283437fb5d791e
    
    Change-Id: Ia91835ec7450d8f959811ba562cab98e41df07a3
    Reviewed-by: Shawn Rutledge <shawn.rutledge@qt.io>

--- tests/auto/widgets/widgets/qlineedit/tst_qlineedit.cpp
+++ tests/auto/widgets/widgets/qlineedit/tst_qlineedit.cpp
@@ -5096,9 +5096,6 @@ void tst_QLineEdit::testQuickSelectionWithMouse()
     QVERIFY(lineEdit.selectedText().endsWith(suffix));
     QTest::mouseMove(lineEdit.windowHandle(), center + QPoint(20, 0));
     qCDebug(lcTests) << "Selected text:" << lineEdit.selectedText();
-#if defined(Q_PROCESSOR_ARM_32) && !defined(Q_OS_VXWORKS)
-    QEXPECT_FAIL("", "Currently fails on gcc-armv7, needs investigation.", Continue);
-#endif
     QCOMPARE(lineEdit.selectedText(), partialSelection);
     mouseReleaseIfNeeded(center + QPoint(20, 0));
 
