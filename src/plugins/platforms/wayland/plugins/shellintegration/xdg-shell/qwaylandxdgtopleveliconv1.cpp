c1f7739cf8794b48cde07fef78b3f46001da7984
Jie Liu <liujie01@kylinos.cn>
2025-10-23 10:32:52 +0800

    wayland: remove duplicate include of qwaylandshmbackingstore_p.h
    The file already includes <QtWaylandClient/private/qwaylandshmbackingstore_p.h>,
    so the local include "qwaylandshmbackingstore_p.h" was redundant and has been
    removed to avoid duplicate inclusion.
    
    Change-Id: If3596e233ed90c2a04c736cb93056aa58dfd34e1
    Reviewed-by: David Redondo <qt@david-redondo.de>

--- src/plugins/platforms/wayland/plugins/shellintegration/xdg-shell/qwaylandxdgtopleveliconv1.cpp
+++ src/plugins/platforms/wayland/plugins/shellintegration/xdg-shell/qwaylandxdgtopleveliconv1.cpp
@@ -2,7 +2,6 @@
 // Copyright (C) 2024 David Edmundson <davidedmundson@kde.org>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#include "qwaylandshmbackingstore_p.h"
 #include "qwaylandxdgtopleveliconv1_p.h"
 
 #include <QtWaylandClient/private/qwaylanddisplay_p.h>
