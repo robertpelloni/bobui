da9022d7abf8317b2cda47f10986b1e250d60d76
David Boddie <david.boddie@qt.io>
2025-12-17 17:07:57 +0100

    Doc: Use a styled SVG image instead of a PNG for a model index diagram
    This should adapt to dark themes in browsers and in Qt Assistant.
    
    Change-Id: I198031455b78fc12d69824de0af4fa7bed46f8a1
    Reviewed-by: Andreas Eliasson <andreas.eliasson@qt.io>

--- src/corelib/itemmodels/qabstractitemmodel.cpp
+++ src/corelib/itemmodels/qabstractitemmodel.cpp
@@ -1390,7 +1390,7 @@ void QAbstractItemModel::resetInternalData()
     simple table of rows and columns. Each item has a unique index specified by
     a QModelIndex.
 
-    \image modelindex-no-parent.png {Screenshot showing a 3x3 grid with numbered
+    \image modelindex-no-parent.svg {Diagram showing a 3x3 grid with numbered
            rows and columns that shows the cell at row 1, column 2 highlighted.}
 
     Every item of data that can be accessed via a model has an associated model
